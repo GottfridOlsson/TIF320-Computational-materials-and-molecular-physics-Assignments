f31f73c1d52b50d82788c10f8296ce18eb7f8515
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-01-19 09:52:36 +0100

    Update Task 2.py

--- Assignment 1/Python/Task 2.py
+++ Assignment 1/Python/Task 2.py
@@ -11,5 +11,11 @@ def create_discretized_1D_space(start, end, number_of_points):
     linspace = [a+i*(b-a)/n for i in range(n+1)]
     return linspace
 
+def Hartree_potential(radius):
+    r = radius
+    return (1/r) - (1 + 1/r)*np.exp(-2*r)
+
+def create_matrix_A_task_2(number_of_points_in_discretized_1D_grid):
+    return 0
 
 print(create_discretized_1D_space(5, 10, 7))
