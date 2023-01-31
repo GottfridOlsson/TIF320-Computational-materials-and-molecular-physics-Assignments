72a34a2b00fb2b913c6b062aa7eada5eb34a214e
Viktor Lilja <viktorlilja2@gmail.com>
2023-01-31 11:58:51 +0100

    Problem 5 generator images and xyz

--- Assignment 2/problem_5/structure_to_xyz.py
+++ Assignment 2/problem_5/structure_to_xyz.py
@@ -1,7 +1,17 @@
 from ase.db import connect
 from ase.io import write
 
-db = connect('../problem_1/natoms_6/gadb.db')
-atoms = db.get('id=79').toatoms ()
-write('./structures/natoms_6.xyz', atoms)
-print(atoms.structure)
+for natoms in [6, 7, 8]:
+
+    # Connect to database
+    db = connect(f'./TIF320-Computational-materials-and-molecular-physics-Assignments/Assignment 2/problem_1/natoms_{natoms}/gadb.db')
+
+    # Create a iterator that returns configurations sorted by energy
+    atoms_iterator = db.select(sort="energy")
+
+    # Get the first value of the iterator
+    atoms =  next(atoms_iterator).toatoms()
+
+    # Save the result
+    write(f'./TIF320-Computational-materials-and-molecular-physics-Assignments/Assignment 2/problem_5/structures/natoms_{natoms}.xyz', atoms)
+    write(f'./TIF320-Computational-materials-and-molecular-physics-Assignments/Assignment 2/problem_5/structures/natoms_{natoms}.png', atoms)
