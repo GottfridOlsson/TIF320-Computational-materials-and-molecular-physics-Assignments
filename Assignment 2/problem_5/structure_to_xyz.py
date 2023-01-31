3adfcea5d3b34b2f9d30cc3b4b3b8c11c071726d
Viktor Lilja <viktorlilja2@gmail.com>
2023-01-31 12:32:10 +0100

    Generated some new structures

--- Assignment 2/problem_5/structure_to_xyz.py
+++ Assignment 2/problem_5/structure_to_xyz.py
@@ -3,15 +3,23 @@ from ase.io import write
 
 for natoms in [6, 7, 8]:
 
+    assignment_path = './TIF320-Computational-materials-and-molecular-physics-Assignments/Assignment 2'
+    db_path = assignment_path + f'/problem_1/natoms_{natoms}/gadb.db'
+    save_path = assignment_path + f'/problem_5/structures/natoms_{natoms}'
+
     # Connect to database
-    db = connect(f'./TIF320-Computational-materials-and-molecular-physics-Assignments/Assignment 2/problem_1/natoms_{natoms}/gadb.db')
+    db = connect(db_path)
 
     # Create a iterator that returns configurations sorted by energy
-    atoms_iterator = db.select(sort="energy")
+    atoms_iterator_gpaw = db.select("calculator=gpaw", sort="energy")
+    atoms_iterator_lj = db.select("calculator=lennardjones", sort="energy")
 
-    # Get the first value of the iterator
-    atoms =  next(atoms_iterator).toatoms()
+    # Get the first value of the iterator as groundstate
+    atoms_groundstate_gpaw =  next(atoms_iterator_gpaw).toatoms()
+    atoms_groundstate_lj =  next(atoms_iterator_lj).toatoms()
 
     # Save the result
-    write(f'./TIF320-Computational-materials-and-molecular-physics-Assignments/Assignment 2/problem_5/structures/natoms_{natoms}.xyz', atoms)
-    write(f'./TIF320-Computational-materials-and-molecular-physics-Assignments/Assignment 2/problem_5/structures/natoms_{natoms}.png', atoms)
+    write(save_path + '_groundstate_gpaw.xyz', atoms_groundstate_gpaw)
+    write(save_path + '_groundstate_gpaw.png', atoms_groundstate_gpaw)
+    write(save_path + '_groundstate_lennardjones.xyz', atoms_groundstate_lj)
+    write(save_path + '_groundstate_lennardjones.png', atoms_groundstate_lj)
