8beafff9a4276edec3a4a10d80f4e9a2d88cbc7b
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-01 16:10:27 +0100

    Cube files for wavefunctions

--- Assignment 2/problem_10/wavefunctions.py
+++ Assignment 2/problem_10/wavefunctions.py
@@ -14,13 +14,9 @@
 # 7. save the wavefunction as cube file
 # 8. try open the cube file in PyMOL to see if it works
 
-
-# test: https://wiki.fysik.dtu.dk/gpaw/tutorialsexercises/wavefunctions/plotting/plot_wave_functions.html#creating-wave-function-cube-files
-
 from ase.io import write, read
 from ase.units import Bohr
-from gpaw import restart
-from ase import Atoms
+#from gpaw import restart
 from gpaw import GPAW
 
 # Declare paths
@@ -29,32 +25,21 @@ problem5_path = './Assignment 2/problem_5'
 xyz_path_start = f'{problem5_path}/structures/natoms_'
 xyz_path_end = '_groundstate_gpaw.xyz'
 
-# atoms = read(xyz_path)
+for natoms in [6, 7, 8]:
 
-
-for natoms in [6]:
-
-    # Read stable configuration files
+    # Read xyz files for the most stable configurations
     xyz_path = xyz_path_start + str(natoms) + xyz_path_end
     atoms = read(xyz_path)
 
-    # Set GPAW and calculate
-    calc = GPAW(nbands=natoms, h=0.2, txt=None, setups={'Na': '1'})
+    # Set calculator as GPAW
+    calc = GPAW(nbands=0, h=0.2, txt=None, setups={'Na': '1'}) # nbands = 0 ("nbands=0 will give zero empty bands, and nbands=-n will give n empty bands.") #https://wiki.fysik.dtu.dk/gpaw/documentation/basic.html
     atoms.calc = calc
-    energy = atoms.get_potential_energy()
-
-    # Save wave functions
-    #saved_wavefunction_path = problem10_path + '/wavefunctions/' + str(natoms) + '_.gpw'
-    #calc.write(saved_wavefunction_path, mode='all')
-    #print(f"wrote wavefunctions for natoms={natoms} to path: {saved_wavefunction_path}")
-
-    # Load binary file and get calculator (this might be unnecessary, but kept while testing stuff)
-    #atoms, calc = restart(saved_wavefunction_path)
+    energy = atoms.get_potential_energy() #it complains for natoms=6 if this is not here (do not know why)
 
     # Write wavefunctions to cube files
     nbands = calc.get_number_of_bands()
     for band in range(nbands):
-        wf = calc.get_pseudo_wave_function(band=band)
-        fname = f'Na{natoms}_band_{band}.cube'
-        print('writing wf', band, 'to file: ' + problem10_path, fname)
-        write(problem10_path + '/cubefiles/'+ fname, atoms, data=wf * Bohr**1.5)
+        wavefunction = calc.get_pseudo_wave_function(band=band)
+        cube_file_path = problem10_path + f'/cubefiles/Na{natoms}_band_number_{band+1}.cube'
+        print(f'Writing wavefunction for Na{natoms} for band number ' + str(band) + f' to file: {cube_file_path}')
+        write(cube_file_path, atoms, data=wavefunction * Bohr**1.5)
