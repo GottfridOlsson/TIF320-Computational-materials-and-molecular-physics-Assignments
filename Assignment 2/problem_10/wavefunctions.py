7236ca3a61e13d5212053464b0360f609a17820e
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-02 08:41:38 +0100

    Cube wavefunctions

--- Assignment 2/problem_10/wavefunctions.py
+++ Assignment 2/problem_10/wavefunctions.py
@@ -8,13 +8,14 @@ xyz_path_start = f'{problem5_path}/structures/natoms_'
 xyz_path_end = '_groundstate_gpaw.xyz'
 
 for natoms in [6, 7, 8]:
+    print(f"Calculating for natoms={natoms}")
 
     # Read xyz files for the most stable configurations
     xyz_path = xyz_path_start + str(natoms) + xyz_path_end
     atoms = read(xyz_path)
 
     # Set calculator as GPAW
-    calc = GPAW(nbands=0, h=0.2, txt=None, setups={'Na': '1'}) # nbands = 0 ("nbands=0 will give zero empty bands, and nbands=-n will give n empty bands.") #https://wiki.fysik.dtu.dk/gpaw/documentation/basic.html
+    calc = GPAW(nbands=5, h=0.2, setups={'Na': '1'}, mode='lcao', basis='dzp', txt=None) # nbands = 0 ("nbands=0 will give zero empty bands, and nbands=-n will give n empty bands.") #https://wiki.fysik.dtu.dk/gpaw/documentation/basic.html
     atoms.calc = calc
     energy = atoms.get_potential_energy() #it complains for natoms=6 if this is not here (do not know why)
 
@@ -22,6 +23,6 @@ for natoms in [6, 7, 8]:
     nbands = calc.get_number_of_bands()
     for band in range(nbands):
         wavefunction = calc.get_pseudo_wave_function(band=band)
-        cube_file_path = problem10_path + f'/cubefiles/Na{natoms}_band_number_{band+1}.cube'
+        cube_file_path = problem10_path + f'/cubefiles/Na{natoms}_band_number_{band}.cube'
         print(f'Writing wavefunction for Na{natoms} for band number ' + str(band) + f' to file: {cube_file_path}')
         write(cube_file_path, atoms, data=wavefunction * Bohr**1.5)
