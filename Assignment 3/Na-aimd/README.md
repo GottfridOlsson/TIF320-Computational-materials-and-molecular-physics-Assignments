d7c298d84ff796bf6987def730c34bd15ad92442
Viktor Lilja <viktorlilja2@gmail.com>
2023-02-07 08:23:59 +0100

    Added documents for assignment 3

---
+++ Assignment 3/Na-aimd/README.md
@@ -0,0 +1,5 @@
+cluster24 contains results from a 6 ps AIMD simulation of 24 water molecules.
+NaCluster 24 contains results from a 7 ps AIMD simulation of 24 water molecules with a sodium ion.
+
+.log contains temperature at each timestep.
+.traj contains the positions (and velocities...) at each timestep.
---
+++ Assignment 3/run-aimd.py
@@ -0,0 +1,32 @@
+import numpy as np
+from ase import NPT
+from ase.units import fs, kB
+from gpaw import GPAW
+
+atoms = read('someStartConfiguration.xyz')
+             
+calc = GPAW(
+    #...
+    mode = 'lcao',
+    xc = 'PBE',
+    basis = 'dzp',
+    symmetry= {'point_group ': False}, # Turn off point -group symmetry
+    charge = 1, # Charged system
+    txt = 'output.gpaw -out', # Redirects calculator output to this file!
+)
+
+atoms.set_calculator(calc)
+
+
+dyn = NPT( # Some MD method
+    #...
+    atoms ,
+    temperature_K = 350,
+    timestep = 1000*fs, # This is not an appropriate timestep , I can tell you that!
+    ttime = 20*fs, # Don’t forget the fs!
+    externalstress = 0, # We don’t use the barostat , but this needs to be set anyway!
+    logfile = 'mdOutput.log', # Outputs temperature (and more) to file at each timestep
+    )
+trajectory = Trajectory('someDynamics.traj', 'w', atoms)
+dyn.attach(trajectory.write , interval =1) # Write the current positions etc. to file each timestep
+dyn.run (10) # Run 10 steps of MD simulation
