63665508181643df7ab2eb03d3b39e262d91e0f5
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-09 09:12:31 +0100

    Update

--- Assignment 3/RDF_histogram.py
+++ Assignment 3/RDF_histogram.py
@@ -3,8 +3,8 @@ from ase.io.trajectory import Trajectory
 import util
 
 # Read trajectories for cluster with Na
-trajectories = Trajectory('Assignment 3/Na-aimd/NaCluster24.traj') # 'Assignment 3/logs/nose_hoover_trajectory.traj'
-first_trajectory, last_trajectory = int(len(trajectories)*(1/7)),len(trajectories) # our simulation: int(len(trajectories)*(0.5/2)),len(trajectories) 
+trajectories = Trajectory('Assignment 3/Na-aimd/cluster24.traj') # 'Assignment 3/logs/nose_hoover_trajectory.traj'
+first_trajectory, last_trajectory = int(len(trajectories)*(1/6)),len(trajectories) # our simulation: (0.5/2), their with Na: (1/7), their without Na: (1/6)
 
 # Create histogram to save the RDF to
 cell_length = trajectories[0].cell[0][0]
@@ -21,19 +21,21 @@ for atoms in trajectories[first_trajectory:last_trajectory]:
 
     # Select index for Na and O atoms
     chemical_symbols = np.array(atoms.get_chemical_symbols())
-    Na_atom_index    = np.where(chemical_symbols=='Na')[0]
+    #Na_atom_index    = np.where(chemical_symbols=='Na')[0]
     O_atoms_indexes  = np.where(chemical_symbols=='O')[0]
-    
-    # Get distance (angstrom) between atoms using the minimum image convention (mic)
-    distances = atoms.get_distances(Na_atom_index, O_atoms_indexes, mic=True) 
-    
-    # Add distances to histogram to produce radial distribution function (RDF)   
-    for distance in distances:
-        if distance < r_min or distance > r_max:
-            continue
 
-        bin_index = int(np.floor( (distance-r_min)/binwidth ))
-        histogram[bin_index] += 1
+    for O_atom_index in O_atoms_indexes:
+        
+        # Get distance (angstrom) between atoms using the minimum image convention (mic)
+        distances = atoms.get_distances(O_atom_index, O_atoms_indexes, mic=True) 
+        
+        # Add distances to histogram to produce radial distribution function (RDF)   
+        for distance in distances:
+            if distance < r_min or distance > r_max:
+                continue
+
+            bin_index = int(np.floor( (distance-r_min)/binwidth ))
+            histogram[bin_index] += 1
 
 
 # Scale histogram by average density, by volume of spherical shells, and by number of time steps used
@@ -42,7 +44,7 @@ histogram_scaled = histogram_scaled / (4*np.pi*radial_coordinates**2*binwidth)
 histogram_scaled = histogram_scaled / trajectory_number
 
 # Save histogram to CSV
-util.print_arrays_to_CSV("Assignment 3/TIF320_A3_RDF_histogram_their_simulation.csv", 
+util.print_arrays_to_CSV("Assignment 3/TIF320_A3_RDF_histogram_their_simulation_without_Na.csv", 
                             "Radial coordinate (angstrom)", radial_coordinates, 
                             "Histogram of radial distances (counts)", histogram,
                             "Scaled histogram (counts)", histogram_scaled)
--- Assignment 3/plot-equilibration.py
+++ Assignment 3/plot-equilibration.py
@@ -3,7 +3,7 @@ from util import *
 import matplotlib.pyplot as plt
 
 # Read data from log file
-data = np.genfromtxt('Assignment 3/Na-aimd/NaCluster24.log', skip_header=1)
+data = np.genfromtxt('Assignment 3/Na-aimd/cluster24.log', skip_header=1)
 time = data[:,0]
 Etot = data[:,1]
 Epot = data[:,2]
@@ -14,17 +14,18 @@ T_instant = T
 T_avg     = np.cumsum(T) / np.cumsum(np.ones_like(T))
 T_target  = 350 * np.ones_like(time)
 
-plt.plot(time, T_instant,   label="Instantaneous temperature")
-plt.plot(time, T_avg,       label="Running average")
-plt.plot(time, T_target,    "k--", label=f"T = {T_target[0]}K")
+#plt.plot(time, T_instant,   label="Instantaneous temperature")
+#plt.plot(time, T_avg,       label="Running average")
+#plt.plot(time, T_target,    "k--", label=f"T = {T_target[0]}K")
+plt.plot(time, Etot,         "k", label=f"E_tot")
 plt.grid()
 plt.legend()
-plt.ylim(200, 500)
+plt.xlim(0, 3)
 plt.xlabel("Time (ps)")
 plt.ylabel("Temperature (K)")
-plt.savefig('Assignment 3/Na-aimd/NaCluster24-simulation-temperature-vs-time.pdf')
+plt.savefig('Assignment 3/Na-aimd/cluster24-simulation-temperature-vs-time.pdf')
 
-print_arrays_to_CSV('Assignment 3/Na-aimd/their-simulation-temperature-energy-vs-time.csv',
+print_arrays_to_CSV('Assignment 3/Na-aimd/their-simulation-without-Na-temperature-energy-vs-time.csv',
     'Time (ps)', time,
     'Instantaneous temperature (K)', T_instant,
     'Running average temperature (K)', T_avg,
