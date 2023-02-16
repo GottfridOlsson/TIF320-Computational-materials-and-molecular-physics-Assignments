ab0239c0ed62cb2d34705798ce2ded6226549a99
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-16 10:54:54 +0100

    Task 3,4 begun

--- Assignment 4/task3.py
+++ Assignment 4/task3.py
@@ -1,17 +1,21 @@
 # Construct surface
 # Example from https://wiki.fysik.dtu.dk/gpaw/tutorialsexercises/structureoptimization/surface/surface.html
+# also: https://wiki.fysik.dtu.dk/ase/tutorials/db/db.html
 
 from ase.build import fcc111
 from ase.io import write
 from gpaw import GPAW, PW
+from ase.optimize import GPMin
+import util
+
 
 elements = ['Au', 'Pt', 'Rh']
 a_from_T1 = [4.177, 3.970, 3.840] #angstrom
-output_path_start = 'Assignment 4/output_T2/'
-
-
+output_path_start = 'Assignment 4/output_T3/'
+potential_energy_of_surface = []
 
 for i, element in enumerate(elements):
+    print(f"i={i}, element={element}")
 
     # Construct (111)-surface for Au, Pt, Rh
     # 3-layered 3X3 surface cell, 6 angstrom of vacuum in +-z-direction
@@ -20,10 +24,27 @@ for i, element in enumerate(elements):
 
     # Calculator
     k = (4, 4, 1) #k-sampling according to problem description
-    cutoff_energy = 450 # eV
-    calc = GPAW(mode=PW(cutoff_energy), kpts=k, txt=f"{output_path_start}{element}_111-surface_calc.txt")
+    cutoff_energy = 200 #450 # eV
+    calc = GPAW(mode=PW(cutoff_energy),
+                kpts=k,
+                txt=f"{output_path_start}{element}_111-surface_calc.txt")
 
-    # Relax surfaces, determine the surface energy for each metal
-    # TODO
+    # Relax surface (code and settings from Assignment 2, 'relaxation.py')
+    surface.set_calculator(calc)
+    dyn = GPMin(surface, 
+                #trajectory='write_path_here.traj', 
+                logfile=f"Assignment 4/output_T3/GPMin_{element}_111-surface.log")
+    dyn.run(fmax=0.1, steps=2) #0.001, 100
+    write(f"{output_path_start}{element}_111-surface_relaxed.png", surface)
+    calc.write(f"{output_path_start}{element}_111-surface_relaxed.gpaw")
+    
+    # Get surface energy (=potential energy?)
+    potential_energy_of_surface.append(surface.get_potential_energy())
+
+
+util.print_arrays_to_CSV("Assignment 4/output_T3/TIF320_A4_T3_surface_energy_Au-Pt-Rh.csv", 
+                            "Element symbol", elements, 
+                            "Potential energy of surface (eV)", potential_energy_of_surface, 
+                            print_message=True)
     
 
--- Assignment 4/task4.py
+++ Assignment 4/task4.py
@@ -1,12 +1,42 @@
 # Construct CO molecule:
-# https://wiki.fysik.dtu.dk/gpaw/tutorialsexercises/wavefunctions/wavefunctions/wavefunctions.html
+
+"""
+Calculate the energy of the O2 molecule and the CO molecule.
+Your k-point sampling should consist only of the gamma-point.
+
+Hint 2: Are the molecules spin-polarized?
+"""
 from ase.build import molecule
-CO = molecule('CO')
+from ase.io import write
+from gpaw import GPAW, PW
+import util
 
+molecule_names = ['CO', 'O2']
 cell_length = 12 #angstrom
+cutoff_energy = 450 #electron volt
+output_path_start = "Assignment 4/output_T4/"
+potential_energy = []
 
-atom = Atoms('O', cell=[6, 6, 6], pbc=False)
-atom.center()
+for molecule_name in molecule_names:
 
-grid_spacing = 0.2 #anstrom
+    # Create molecule as atoms object, center, and set calculator
+    atoms = molecule(molecule_name, 
+                    cell=(cell_length, cell_length, cell_length))
+    atoms.center()
+    write(f"{output_path_start}{molecule_name}_initial_molecule_structure.png", atoms, rotation='10z,-80x')
+    
+    calc = GPAW(mode=PW(cutoff_energy),
+                kpts={'gamma': True},
+                txt=f"{output_path_start}{molecule_name}_calc.txt")
+
+    atoms.set_calculator(calc)
+    potential_energy.append(atoms.get_potential_energy())
+
+    # Spin polarized: https://wiki.fysik.dtu.dk/gpaw/documentation/basic.html (GPAW will take care of that?)
+
+util.print_arrays_to_CSV(f"{output_path_start}TIF320_A4_T4_potential-energy_of_CO_O2_gas.csv", 
+                        "Molecule symbol", molecule_names, 
+                        "Potential energy (eV)", potential_energy, 
+                        print_message=True)
+
 
