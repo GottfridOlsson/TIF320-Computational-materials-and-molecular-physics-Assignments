47b8e9c2552c586af65863d9a54086437c4b8360
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-16 14:40:43 +0100

    Update

--- Assignment 4/run-T3-on-cluster.sh
+++ Assignment 4/run-T3-on-cluster.sh
@@ -2,10 +2,10 @@
 #SBATCH -A C3SE2023-2-2                    # Project
 #SBATCH -J A4_T3                           # Name of the job
 #SBATCH -N 1                               # Use 1 node
-#SBATCH -n 2                               # Use 2 cores on that node
-#SBATCH -t 02:00:00                        # Maximum time
-#SBATCH -o "Assignment 4/logs/T3_std.out"   # stdout goes to this file
-#SBATCH -e "Assignment 4/logs/T3_err.out"   # stderr goes to this file
+#SBATCH -n 16                              # Use 16 cores on that node
+#SBATCH -t 00:45:00                        # Maximum time
+#SBATCH -o "Assignment 4/logs/T3_std.out"  # stdout goes to this file
+#SBATCH -e "Assignment 4/logs/T3_err.out"  # stderr goes to this file
 
 # Load modules
 module purge
@@ -13,5 +13,5 @@ module load SciPy-bundle/2022.05-foss-2022a
 module load ASE/3.22.1-foss-2022a
 module load GPAW/22.8.0-foss-2022a
 
-python3 'Assignment 4/task3.py'
+mpiexec gpaw python 'Assignment 4/task3.py'
 
