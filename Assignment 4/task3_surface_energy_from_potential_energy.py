d904482de10c9a423e86acd0411d1ae4f94f562c
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-17 10:07:11 +0100

    T3 calculations done

--- Assignment 4/task3_surface_energy_from_potential_energy.py
+++ Assignment 4/task3_surface_energy_from_potential_energy.py
@@ -2,7 +2,7 @@ import numpy as np
 
 elements = ['Au', 'Pt', 'Rh']
 E_bulk = [-3.146, -6.434, -7.307] # eV/atom from Task 1
-E_slab = [-106.52714481388658, -195.846074] # eV from Task 3
+E_slab = [-106.52714481388658, -195.846034, -217.5149372] # eV from Task 3
 cell_lenghts = [8.860755, 8.421642, 8.145870] # angstrom
 angles = np.array([60, 60, 60])*(np.pi/180) # degree
 N = 27 #atoms per slab = 3*3*3
@@ -17,5 +17,5 @@ for i, element in enumerate(elements):
 print("Surface energy")
 print(f"Element     E_surface (eV/Å^2)")
 for i, element in enumerate(elements):
-    E_surface = (E_slab[i] - N*E_bulk[i])/(2*area[i])
+    E_surface = (E_slab[i] - N*E_bulk[i]) / (2*area[i])
     print(f"{element}           {E_surface:.4f}")
--- Assignment 4/task5.py
+++ Assignment 4/task5.py
@@ -1,8 +1,47 @@
-# Vibrational energy:
+# Adapted from:
 # https://wiki.fysik.dtu.dk/ase/ase/vibrations/modes.html
-
 # https://wiki.fysik.dtu.dk/ase/ase/thermochemistry/thermochemistry.html#ase.thermochemistry.CrystalThermo.get_entropy
 
+from ase.thermochemistry import IdealGasThermo
+from ase.vibrations import Vibrations
+from ase.calculators.emt import EMT
+from ase.optimize import QuasiNewton
+from ase.build import molecule
+
+molecule_names = ['CO', 'O2']
+symmetry_numbers = [1, 2]
+spins = [0, 1] # singlet and triplet for CO and O2 respectively
+
+# Temperature and pressure
+temperature = 300 # Kelvin
+pressure = 1*1e5 # bar*1e5 = Pa
+
+for i in len(molecule_names):
+    
+    molecule_name = molecule_names[i]
+    symmetry_number = symmetry_numbers[i]
+    spin = spins[i]
+
+    # Create molecule
+    atoms = molecule(molecule_name)
+    atoms.calc = EMT()
+    dyn = QuasiNewton(atoms)
+    dyn.run(fmax=0.01)
+    potentialenergy = atoms.get_potential_energy()
+
+    # Set up and run vibrational analysis
+    vib = Vibrations(atoms)
+    vib.run()
 
 
-atoms.get_entropy()
+    # Calculate quantities
+    potential_energy = atoms.get_potential_energy() # eV
+    vibrational_energies = vib.get_energies() # eV
+   
+    ideal_gas = IdealGasThermo(vib_energies=vibrational_energies, 
+                                geometry='linear',
+                                potentialenergy=potential_energy,
+                                atoms=atoms,
+                                symmetrynumber=symmetry_number,
+                                spin=spin)
+    entropy = ideal_gas.get_entropy(temperature=temperature,pressure=pressure) # eV/K
