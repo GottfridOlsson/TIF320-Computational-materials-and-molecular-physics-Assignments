97c4fcf1d6044745e08c4b931334f108a89fc94a
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-22 09:19:24 +0100

    Create task6.py

---
+++ Assignment 4/task6.py
@@ -0,0 +1,13 @@
+# Inspired by:
+# https://wiki.fysik.dtu.dk/ase/tutorials/db/db.html
+
+from ase.io import read
+
+
+
+
+# Read relaxed surfaces from Task 3
+Au_surface = read('Assignment 4/output_T3/Au_111-surface_relaxed.xyz')
+Pt_surface = read('Assignment 4/output_T3/Pt_111-surface_relaxed.xyz')
+Rh_surface = read('Assignment 4/output_T3/Rh_111-surface_relaxed.xyz')
+
