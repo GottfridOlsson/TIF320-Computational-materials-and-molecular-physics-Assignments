5a5d2266e1861b1e63125025a9eedbe603500ab4
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-23 12:32:40 +0100

    Update T6 adsorption energy

--- Assignment 4/task6_E_activation.py
+++ Assignment 4/task6_E_activation.py
@@ -24,7 +24,7 @@ adsorbate_names = ["CO", "O"]
 E_adsorbate_CO  = -14.194 #eV, from Task 4
 E_adsorbate_O2  = -8.728  #eV, from Task 4
 
-positions = ['ontop', 'bridge', 'fcc', 'hcp']
+positions = ['fcc', 'hcp', 'ontop', 'bridge']
 output_path_start = "Assignment 4/output_T6/"
 
 with paropen(f"{output_path_start}E_activation_CO_O.txt", 'w') as file:
@@ -37,36 +37,52 @@ with paropen(f"{output_path_start}E_adsorption.txt", 'w') as file:
 for i, surface_name in enumerate(surface_names):
 
     # 3-layered 3X3 surface cell, 6 angstrom of vacuum in +-z-direction
-    surface = fcc111(surface_name, (3, 3, 3), a=a_from_T1[i], vacuum=6.0)
-    calc = GPAW(xc='PBE',
-            mode=PW(450),
-            kpts=(4,4,1), 
-            txt=f"{output_path_start}{surface_name}_GPAW_unrelaxed.txt")
-    surface.set_calculator(calc)
+    #surface = fcc111(surface_name, (3, 3, 3), a=a_from_T1[i], vacuum=6.0)
+    #calc = GPAW(xc='PBE',
+    #        mode=PW(450),
+    #        kpts=(4,4,1), 
+    #        txt=f"{output_path_start}{surface_name}_GPAW_unrelaxed.txt")
+    #surface.set_calculator(calc)
+    surface = read(f"{output_path_start}{surface_name}_fcc111_initialized.xyz")
     E_slab = surface.get_potential_energy()
-    print(f"{surface_name}, E_slab={E_slab}")
-    write(f"{output_path_start}{surface_name}_fcc111_initialized.xyz", surface)
-    
+    #print(f"{surface_name}, E_slab={E_slab}")
+    #write(f"{output_path_start}{surface_name}_fcc111_initialized.xyz", surface)
 
-    for position in positions:
-
-        # Slab with adsorbate energy
-        O  = read(f"{output_path_start}{surface_name}_O_{position}_relaxed.xyz")
-        CO = read(f"{output_path_start}{surface_name}_CO_{position}_relaxed.xyz")
-        E_slabWithAdsorbate_O  = O.get_potential_energy()
-        E_slabWithAdsorbate_CO = CO.get_potential_energy()
-
-        # Adsorption energy
-        E_ads_O  = adsorption_energy_CO(E_slabWithAdsorbate_O, E_slab, E_adsorbate_O2)
-        E_ads_CO = adsorption_energy_CO(E_slabWithAdsorbate_CO, E_slab, E_adsorbate_CO)
-
-        # Activation energy
-        E_act = activation_energy(E_ads_O, E_ads_CO)
-
-        with paropen(f"{output_path_start}E_activation_CO_O.txt", 'a') as file:
-            file.write(f"{surface_name}, {position}, {E_act}\n")
-
+    for adsorbate in ['CO', 'O']:
         with paropen(f"{output_path_start}E_adsorption.txt", 'a') as file:
-            file.write(f"{surface_name}, CO, {position}, {E_ads_CO}\n")
-            file.write(f"{surface_name}, O,  {position}, {E_ads_O}\n")
+            file.write(f"{surface_name}, {adsorbate}\n")
+            file.write(f"'fcc', 'hcp', 'ontop', 'bridge'\n")
+
+        for position in positions:
+
+            # Slab with adsorbate energy
+            if adsorbate=='CO':
+                CO = read(f"{output_path_start}{surface_name}_CO_{position}_relaxed.xyz")
+                E_slabWithAdsorbate_CO = CO.get_potential_energy()
+                E_ads_CO = adsorption_energy_CO(E_slabWithAdsorbate_CO, E_slab, E_adsorbate_CO)
+                E_ads = E_ads_CO
             
+            if adsorbate=='O':
+                O  = read(f"{output_path_start}{surface_name}_O_{position}_relaxed.xyz")
+                E_slabWithAdsorbate_O  = O.get_potential_energy()
+                E_ads_O  = adsorption_energy_O(E_slabWithAdsorbate_O, E_slab, E_adsorbate_O2)
+                E_ads = E_ads_O
+
+            # Adsorption energy
+            
+            
+
+            # Activation energy
+            #E_act = activation_energy(E_ads_O, E_ads_CO)
+
+            #with paropen(f"{output_path_start}E_activation_CO_O.txt", 'a') as file:
+            #    file.write(f"{surface_name}, {position}, {E_act}\n")
+
+            with paropen(f"{output_path_start}E_adsorption.txt", 'a') as file:
+                file.write("\\num{" +f"{E_ads:.4}" + "}")
+                if position!='bridge':
+                    file.write(" & ")
+                #file.write(f"{surface_name}, CO, {position}, {E_ads_CO}\n")
+                #file.write(f"{surface_name}, O,  {position}, {E_ads_O}\n")
+        with paropen(f"{output_path_start}E_adsorption.txt", 'a') as file:
+            file.write(f"\n\n")
