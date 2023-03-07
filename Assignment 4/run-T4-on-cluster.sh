b50fc07803636576a793edea1ed1e10b781de0ee
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-03-07 15:58:08 +0100

    Pre calc

---
+++ Assignment 4/run-T4-on-cluster.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#SBATCH -A C3SE2023-2-2                    # Project
+#SBATCH -J A4_T4                           # Name of the job
+#SBATCH -N 1                               # Use 1 node
+#SBATCH -n 4                               # Use 16 cores on that node
+#SBATCH -t 01:00:00                        # Maximum time
+#SBATCH -o "logs/T4_std.out"  # stdout goes to this file
+#SBATCH -e "logs/T4_err.out"  # stderr goes to this file
+
+# Load modules
+module purge
+module load SciPy-bundle/2022.05-foss-2022a
+module load ASE/3.22.1-foss-2022a
+module load GPAW/22.8.0-foss-2022a
+
+mpiexec gpaw python 'Assignment 4/task4.py'
--- Assignment 4/task4.py
+++ Assignment 4/task4.py
@@ -1,6 +1,7 @@
 from ase.build import molecule
 from ase.io import write
 from gpaw import GPAW, PW
+from ase.optimize import GPMin
 import util
 
 molecule_names = ['CO', 'O2']
@@ -23,12 +24,15 @@ for molecule_name in molecule_names:
                 txt=f"{output_path_start}{molecule_name}_calc.txt")
 
     atoms.set_calculator(calc)
+    dyn = GPMin(atoms, 
+                trajectory=f"{output_path_start}GPMin_{molecule_name}.traj", 
+                logfile=f"{output_path_start}GPMin_{molecule_name}.log")
+    dyn.run(fmax=0.01, steps=25) #0.001, 100
+
     potential_energy.append(atoms.get_potential_energy())
     write(f"{output_path_start}{molecule_name}_relaxed_molecule_structure.xyz", atoms)
 
-    util.print_arrays_to_CSV(f"{output_path_start}TIF320_A4_T4_potential-energy_of_CO_O2_gas.csv", 
+    util.print_arrays_to_CSV(f"{output_path_start}TIF320_A4_T4_potential-energy_of_CO_O2_gas_relaxed.csv", 
                             "Molecule symbol", molecule_names, 
                             "Potential energy (eV)", potential_energy, 
                             print_message=True)
-
-
